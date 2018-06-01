1b1c4117933717a4cb104682b7842ea494095f6e
ajila <img123@126.com>
2018-06-01 10:42:57 +0800

    调整模板结构

--- template-parts/post/content-gallery.php
+++ template-parts/post/content-gallery.php
@@ -14,52 +14,35 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-    <?php if( ! is_single() ) :   //若非单篇文章(即文章列表) ?>
-        <?php if ( get_post_gallery() ): //有画廊，则以slider的方式，显示画廊图片 ?>
+    <?php if( ! is_single() ) :   //若非单篇文章(即文章列表)
+         if ( get_post_gallery() ): //有画廊，则以slider的方式，显示画廊图片 ?>
             <?php $ID = get_the_ID(); ?>
 		    <div class="entry-gallery">
                 <?php get_template_part('template-parts/slider/slider', 'gallery'); ?>
             </div>  <!-- .entry-gallery -->
 
-        <?php elseif('' !== get_the_post_thumbnail() ) : //无画廊有缩略图，则显示缩略图 ?>
+        <?php elseif( has_post_thumbnail() ) : //无画廊有缩略图，则显示缩略图 ?>
             <div class="post-thumbnail">
                 <a href="<?php the_permalink(); ?>">
-				    <?php
-                    //the_post_thumbnail( 'jigim-thumbnail-horizontal' );
-				    jigim_echo_responsive_thumbnail( $post, 'post-list' );
-                    ?>
+				    <?php //jigim_echo_responsive_thumbnail( $post, 'post-list' ); ?>
+                    <img data-src = "<?php get_the_post_thumbnail_url( $post, 'jigim-thumbnail-vertical'); ?>"
+                        class="lazyload" alt="post image attachment">
                 </a>
             </div><!-- .post-thumbnail -->
 
         <?php else: //也无缩略图，则显示文章中第一张图片 ?>
-            <?php $img = jigim_get_post_first_img( get_the_content() ); ?>
-            <div class="post-image-attachment"><a href="<?php the_permalink(); ?>">
-                <img data-src = "<?php jigim_get_post_first_img( get_the_content() );?>" class="lazyload" alt="post image attachment">
-            </a> </div><!-- .post-image-attachment -->
-        <?php endif; ?>
+            <div class="post-image-attachment">
+                <a href="<?php the_permalink(); ?>">
+                    <img data-src = "<?php jigim_get_post_first_img( get_the_content() );?>" class="lazyload" alt="post image attachment">
+                </a>
+            </div><!-- .post-image-attachment -->
+        <?php endif;
+
+
+        //文章meta信息和标题
+        jigim_entry_header();   ?>
+
     <?php endif; ?>
-
-
-	<?php if( !is_single() ): //非单篇文章(文章列表)，显示文章meta信息和标题 ?>
-	<header class="entry-header">
-    <?php
-        //当前是博客主页且是置顶文章，输出图标
-        if ( is_sticky() && is_home() ) {
-            echo '<span class="fa fa-thumb-tack sticky-icon"></span>';
-        }
-
-        //section1: 文章分类
-        jigim_entry_category();
-
-        //section2: 文章标题
-        jigim_entry_title();
-    ?>
-	</header><!-- .entry-header -->
-	<?php endif; ?>
-
-
-	<?php if( is_single()): //TODO：面包屑导航 ?>
-	<?php endif; ?>
 
 
 	<?php //if ( is_single() || (!get_post_gallery() &&  !get_the_post_thumbnail()) ):
@@ -82,20 +65,9 @@
             ) );
 		    ?>
     	</div><!-- .entry-content -->
+    <?php endif; ?>
 
 
-        <footer class="entry-footer">
-            <?php jigim_entry_tag();    //输出tag列表 ?>
-        </footer>
-	<?php else: ?>
-        <footer class="entry-footer">
-			<?php
-			jigim_entry_tag();  //输出tag列表
-			echo '<div class="entry-meta">';
-            jigim_posted_on();  //文章列表时，作者日期时间显示在底部
-			echo '</div>';
-            ?>
-        </footer>
-	<?php endif; ?>
+	<?php jigim_entry_footer(); ?>
 
 </article><!-- #post-## -->
